5932cc02328425697350d995087661add857dac0
Guilherme Loureiro <guilherme631999@gmail.com>
2024-11-22 17:12:58 +0000

    trying something

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
 
                 try {
                     const response = await axios.get(url, {
-                        timeout: 10000, // Short timeout for individual requests
+                        timeout: 20000, // Short timeout for individual requests
                         headers: {
                             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
                         }
